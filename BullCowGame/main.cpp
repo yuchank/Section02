b0b8580785983a9dcb1dbbe63b8e2b95fe12ee42
thexr1868 <thexr1868@outlook.com>
2018-10-23 16:16:41 +0900

    fix lines

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -35,8 +35,14 @@ int main()
 // introduce the game
 void PrintIntro() 
 {
-	std::cout << std::endl << std::endl;
 	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << std::endl;
+	std::cout << "          }   {         ___ " << std::endl;
+	std::cout << "          (o o)        (o o) " << std::endl;
+	std::cout << "   /-------\\ /          \\ /-------\\ " << std::endl;
+	std::cout << "  / | BULL |O            O| COW  | \\ " << std::endl;
+	std::cout << " *  |-,--- |              |------|  * " << std::endl;
+	std::cout << "    ^      ^              ^      ^ " << std::endl;
 	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength() << " letter isogram I'm thinking of?\n";
 	std::cout << std::endl;
 	return;
@@ -76,7 +82,7 @@ FText GetValidGuess()
 	do 
 	{
 		// get a guess from the player
-		std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+		std::cout << "Try " << CurrentTry << " of " << BCGame.GetMaxTries() << ". Enter your guess: ";
 		std::getline(std::cin, Guess);
 
 		Status = BCGame.CheckGuessValidity(Guess);
@@ -84,22 +90,22 @@ FText GetValidGuess()
 		switch (Status)
 		{
 		case EGuessStatus::Not_Isogram:
-			std::cout << "Please enter a word without repeating letters." << std::endl;
+			std::cout << "Please enter a word without repeating letters." << std::endl << std::endl;
 			break;
 		case EGuessStatus::Wrong_Length:
-			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word." << std::endl;
+			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word." << std::endl << std::endl;
 			break;
 		case EGuessStatus::Not_Lowercase:
-			std::cout << "Please enter all lowercase letters." << std::endl;
+			std::cout << "Please enter all lowercase letters." << std::endl << std::endl;
 			break;
 		default:
 			// assume the guess is valid
 			break;
 		}
-		if (std::cin.eof())
+		
+		if (std::cin.eof())		// EOF patch
 			std::cin.clear();
 
-		std::cout << std::endl;
 	} while (Status != EGuessStatus::OK);	// keep looping until we get no errors
 
 	return Guess;
